4aff2d5215a7abb8b53de1d647c2f295fe4de7d4
najczel@gmail.com <najczel@gmail.com>
2023-02-02 23:44:31 +0100

    Zadanie domowe, dzien 4

--- Challenge21/Challenge21/Program.cs
+++ Challenge21/Challenge21/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Marysia";
+var age = 35;
+bool female = true;
+
+if (female && name != "Marysia" && age > 20)
+{
+    Console.WriteLine("NASTOLATKA");
+}
+else if (name == "Marysia" && age == 35)
+{
+    Console.WriteLine("35 latka o imieniu Marysia");
+}
+else if (name == "Marcin" && age < 60 && !female) 
+{
+    Console.WriteLine("Senior o imniu Marcin"); 
+}
+else
+{
+    Console.WriteLine("Osoba nieznana");
+}
+
+    
