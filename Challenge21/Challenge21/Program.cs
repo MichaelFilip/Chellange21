e7ccfb9b377fb8cc2a05ead64df43bcdbab387f5
najczel@gmail.com <najczel@gmail.com>
2023-02-07 00:05:37 +0100

    zadanie domowe z dnia 5

--- Challenge21/Challenge21/Program.cs
+++ Challenge21/Challenge21/Program.cs
@@ -1,22 +1,68 @@
-﻿var name = "Marysia";
-var age = 35;
-bool female = true;
+﻿int number = 2034525568; 
+string numbesAsString  = number.ToString();
+char[] digits = numbesAsString.ToArray();
 
-if (female && name != "Marysia" && age > 20)
-{
-    Console.WriteLine("NASTOLATKA");
-}
-else if (name == "Marysia" && age == 35)
-{
-    Console.WriteLine("35 latka o imieniu Marysia");
-}
-else if (name == "Marcin" && age < 60 && !female) 
-{
-    Console.WriteLine("Senior o imniu Marcin"); 
-}
-else
-{
-    Console.WriteLine("Osoba nieznana");
-}
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-    
+foreach (char digit in digits)
+{
+    if (digit == '0')
+    {
+        counter0++;
+    }
+    else if (digit == '1')
+    {
+        counter1++;
+    }
+    else if (digit == '2')
+    {
+        counter2++;
+    }
+    else if (digit == '3')
+    {
+        counter3++;
+    }
+    else if (digit == '4')
+    {
+        counter4++;
+    }
+    else if (digit == '5')
+    {
+        counter5++;
+    }
+    else if (digit == '6')
+    {
+        counter6++;
+    }
+    else if (digit == '7')
+    {
+        counter7++;
+    }
+    else if (digit == '8')
+    {
+        counter8++;
+    }
+    else if (digit == '9')
+    {
+        counter9++;
+    }
+}
+Console.WriteLine("0 >=" + counter0);
+Console.WriteLine("1 >=" + counter1);
+Console.WriteLine("2 >=" + counter2);
+Console.WriteLine("3 >=" + counter3);
+Console.WriteLine("4 >=" + counter4);
+Console.WriteLine("5 >=" + counter5);
+Console.WriteLine("6 >=" + counter6);
+Console.WriteLine("7 >=" + counter7);
+Console.WriteLine("8 >=" + counter8);
+Console.WriteLine("9 >=" + counter9);
